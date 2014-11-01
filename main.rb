4d3845f2dd19d14c288a96899724bf2874f86853
Daniel Tievsky <Daniel@Daniels-MBP.home>
2014-11-01 17:48:20 -0400

    Read in a json data file.

--- main.rb
+++ main.rb
@@ -1,1 +1,12 @@
-puts " My old message was bad."
+require 'json'
+
+args = ARGV
+if (args.count < 1)
+	puts "Give a data file, please"
+	exit -1
+end
+
+filename = args.first
+
+data_entry = JSON.parse(File.open(filename).read)
+puts data_entry
